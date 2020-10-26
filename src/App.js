de19f4393d71f4cea25c2038fc35c29c77c9b73e
Evgeniy Talko <etalko@gmail.com>
2020-10-26 15:05:57 +0300

    Some markup touches

--- src/App.js
+++ src/App.js
@@ -9,26 +9,35 @@ function App() {
       </header>
       <nav className="navigation">
         <ul>
-          <li>Profile</li>
-          <li>Messages</li>
-          <li>News</li>
-          <li>Music</li>
-          <li>Settings</li>
+          <li><a href="#">Profile</a></li>
+          <li><a href="#">Messages</a></li>
+          <li><a href="#">News</a></li>
+          <li><a href="#">Music</a></li>
+          <li><a href="#">Settings</a></li>
         </ul>
       </nav>
       <div className="content">
+        <div className="page-cover">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Germany-Farchant-Landscape.JPG/800px-Germany-Farchant-Landscape.JPG" alt="Germany Farchant Landscape"/>
+        </div>
         <section className="bio">
           <h2 className="subheading">Ava + Description</h2>
         </section>
-        <section className="post-form-section">
-          <h2 className="subheading">My posts</h2>
-          <form action="echo.htmlacademy.com" className="post-form">
-            <textarea name="text" id="post-text" cols="100" rows="5">
-              ...Type your text
-            </textarea>
-            <button type="submit">Post</button>
-          </form>
-        </section>
+        <div className="my-posts">
+          <section className="post-form-section">
+            <h2 className="subheading">My posts</h2>
+            <form action="echo.htmlacademy.com" className="post-form">
+              <textarea name="text" id="post-text" cols="100" rows="5">
+                ...Type your text
+              </textarea>
+              <button type="submit">Post</button>
+            </form>
+          </section>
+          <section className="posts">
+            <div id="post-1">Post 1</div>
+            <div id="post-2">Post 2</div>
+          </section>
+        </div>
       </div>
     </div>
   );
