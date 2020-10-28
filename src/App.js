8d2e4b7caeaa9817700e84663c07ad6ec944b592
Evgeniy Talko <etalko@gmail.com>
2020-10-28 23:38:25 +0300

    Deleted unneeded components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Navigation from "./components/Navigation";
 import Profile from "./components/Profile";
 
+// Some comment
 function App() {
   return (
     <div className="wrapper">
