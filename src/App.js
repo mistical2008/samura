b21876dd8bb795480976405d70456f16bff28972
Evgeniy Talko <etalko@gmail.com>
2020-10-28 23:43:31 +0300

    some fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
-import Header from "./components/Header";
-import Navigation from "./components/Navigation";
-import Profile from "./components/Profile";
+
+import React from "react";
+
+import Header from "./components/Header/Header";
+import Navigation from "./components/Navigation/Navigation";
+import Profile from "./components/Profile/Profile";
+
 
 // Some comment
 function App() {
