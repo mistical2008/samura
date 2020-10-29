725bb45663b5ec05af74311fbfc5c1faf19e820d
Evgeniy Talko <etalko@gmail.com>
2020-10-29 16:03:03 +0300

    Added util wrapper container

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 import MyPosts from "./MyPosts/MyPosts";
 import ProfileBio from "./ProfileBio/ProfileBio";
-import ProfileHeader from "./ProfileHeader/ProfileHeader";
+import ProfileHeader from "./ProfileCover/ProfileCover";
 import s from "./Profile.module.css"
 
 const Profile = () => {
   return (
     <div className={s.content}>
       <ProfileHeader />
-      <ProfileBio />
-      <MyPosts />
+      <div className={s.wrapper}>
+        <ProfileBio />
+        <MyPosts />
+      </div>
     </div>
   );
 };
