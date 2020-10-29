ababd6a49011d9556872f150349e8b374e0b4e8e
Evgeniy Talko <etalko@gmail.com>
2020-10-29 08:29:00 +0300

    git rebase

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Header from "./components/Header/Header";
 import Navigation from "./components/Navigation/Navigation";
 import Profile from "./components/Profile/Profile";
 
-
-// Some comment
 function App() {
   return (
     <div className="wrapper">
