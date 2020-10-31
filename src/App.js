7ecb41c191309b0af6a083ee688a9fe7134bff31
Evgeniy Talko <etalko@gmail.com>
2020-10-31 15:29:56 +0300

    add routes for nav menu

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import "./App.css";
-
 import React from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import "./App.css";
 
 import Header from "./components/Header/Header";
 import Navigation from "./components/Navigation/Navigation";
-import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import Profile from "./components/Profile/Profile";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
+
+
 
 function App() {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navigation />
-      <div className="app-wrapper-content">
-        <Dialogs />
+    <Router>
+      <div className="app-wrapper">
+        <Header />
+        <Navigation />
+        <div className="app-wrapper-content">
+          <Route path="/dialogs" component={Dialogs}/>
+          <Route path="/profile" component={Profile}/>
+          <Route path="/news" component={News}/>
+          <Route path="/music" component={Music}/>
+          <Route path="/settings" component={Settings}/>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
