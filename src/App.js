fc9483740ede0b10bcd0a6818081f7f492a097f5
Evgeniy Talko <etalko@gmail.com>
2020-11-01 15:56:18 +0300

    Added dialogs components + some styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
 import Header from "./components/Header/Header";
@@ -10,8 +10,6 @@ import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 
-
-
 function App() {
   return (
     <Router>
@@ -19,11 +17,11 @@ function App() {
         <Header />
         <Navigation />
         <div className="app-wrapper-content">
-          <Route path="/dialogs" component={Dialogs}/>
-          <Route path="/profile" component={Profile}/>
-          <Route path="/news" component={News}/>
-          <Route path="/music" component={Music}/>
-          <Route path="/settings" component={Settings}/>
+          <Route exact path="/dialogs" component={Dialogs} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/news" component={News} />
+          <Route exact path="/music" component={Music} />
+          <Route exact path="/settings" component={Settings} />
         </div>
       </div>
     </Router>
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,43 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
+import "../../App.css";
 import s from "./Dialogs.module.css";
 
-const Dialogs = () => {
-  return <div>Dialogs</div>;
+const DialogItem = (props) => {
+  const path = `/dialogs/${props.id}`;
+  return (
+    <div className={s.dialog}>
+      <NavLink to={path} activeClassName={s.activeDialog}>
+        {props.name}
+      </NavLink>
+    </div>
+  );
+};
+
+const Message = (props) => {
+  const className = props.my ? `${s.my} ${s.message}` : `${s.message}`;
+  return <div className={className}>{props.text}</div>;
+};
+
+const Dialogs = (props) => {
+  return (
+    <div className={s.dialogsPage}>
+      <div className={s.dialogs}>
+        <DialogItem name="Dimych" id="1" active />
+        <DialogItem name="Sveta" id="2" />
+        <DialogItem name="Sasha" id="3" />
+        <DialogItem name="Valera" id="4" />
+        <DialogItem name="Anya" id="5" />
+        <DialogItem name="Zhenya" id="6" />
+      </div>
+      <div className={s.messages}>
+        <Message text="Hi there!" />
+        <Message my text="Hello!" />
+        <Message text="How are you?" />
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
