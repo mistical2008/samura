ffc841e2ea0c344114ec0680bdfba3d369fe5ff3
Evgeniy Talko <etalko@gmail.com>
2020-11-05 20:28:44 +0300

    Nothing matters.

--- src/components/Dialogs/DialogItem/DialogItem.jsx
+++ src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -3,7 +3,6 @@ import { NavLink } from "react-router-dom";
 
 import s from "./DialogItem.module.css";
 
-// comment
 const DialogItem = (props) => {
   const path = `/dialogs/${props.id}`;
   return (
