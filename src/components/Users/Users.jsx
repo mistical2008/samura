5ba94320c0233251537b86b3577c11d49bf6c0c9
Evgeniy Talko <etalko@gmail.com>
2020-12-01 15:47:28 +0300

    Users.jsx refactoring

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 const Users = (props) => {
     console.log(props)
     const usersElements = props.users.map(user => <div>
-                <img src={user.avatarUrl} alt={user.fullName} />
-                <ul>
-                    <li>{user.fullName}</li>
-                    <li>{user.description}</li>
-                    <li>
-                        <span>{user.location.country}</span>
-                        <span>{user.location.city}</span>
+                <img src={user.avatarUrl} alt={user.fullName} className={s.avatar} />
+                <ul className={s.userInfo}>
+                    <li className={s.infoItem}>
+                        <h3 className={s.fullName}>{user.fullName}</h3>
+                        <span>{user.description}</span>
+                    </li>
+                    <li className={s.infoItem}>
+                        <span className={s.infoGeo}>{user.location.country}</span>
+                        <span className={s.infoGeo}>{user.location.city}</span>
                     </li>
                 </ul>
             </div>
