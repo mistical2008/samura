49d4bbed0e9d28e33ba94f36e2fb93d1a605acb5
Evgeniy Talko <etalko@gmail.com>
2021-01-11 23:27:19 +0300

    add: useState, useEffect, handleInputKeys util

--- src/components/Profile/ProfileBio/ProfileBio.jsx
+++ src/components/Profile/ProfileBio/ProfileBio.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Preloader from "../../Preloader/Preloader";
-import ProfileStatus from "../../ProfileStatus/ProfileStatus";
+import ProfileStatusWH from "../../ProfileStatus/ProfileStatusWH";
 import Userpic from "../MyPosts/Post/Userpic/Userpic";
 import s from "./ProfileBio.module.css";
 
@@ -12,7 +12,7 @@ const ProfileBio = (props) => {
     <section className={s.bio}>
       <h2 className={s.subheading}>{props.profile.fullName}</h2>
       <Userpic src={props.profile.photos.large} />
-      <ProfileStatus
+      <ProfileStatusWH
         status={props.status}
         updateUserStatus={props.updateUserStatus}
       />
