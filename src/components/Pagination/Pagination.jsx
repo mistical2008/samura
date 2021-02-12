dbc7b115ad035fcc86cf5d263be0a3f8db0e4882
Evgeniy Talko <etalko@gmail.com>
2021-02-12 12:11:45 +0300

    add: pages sections view

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {setState} from "react";
 import s from "./Pagination.module.css";
 
 const Pagination = ({
@@ -6,29 +6,40 @@ const Pagination = ({
   usersPerPage,
   currentPage,
   onPageChanged,
+  sectionSize = 10,
 }) => {
   let pagesCount = Math.ceil(usersCount / usersPerPage);
+  let sectionsCount = pagesCount / sectionSize;
+  const [currentSect, setCurrentSect] = setState(1);
+  let minSectionPage = (currentSect - 1) * sectionSize - 1;
+  let maxSectionPage = currentSect * sectionSize;
   return (
     <ul className={s.pagination}>
-      {[...Array(pagesCount)].map((_, index) => {
-        let number = index + 1;
-        return (
-          <li
-            className={
-              (number === currentPage && s.paginationActiveItem) +
-              " " +
-              s.paginationItem
-            }
-          >
-            <button
-              onClick={(e) => {
-                onPageChanged(number);
-              }}
+      // TODO: refactor array with undefined values to array with numbers.
+      {[...Array(pagesCount)]
+        .filter((_, index) => {
+          const page = index + 1;
+          return page > minSectionPage && page < maxSectionPage;
+        })
+        .map((_, index) => {
+          let number = index + 1;
+          return (
+            <li
+              className={
+                (number === currentPage && s.paginationActiveItem) +
+                " " +
+                s.paginationItem
+              }
             >
-              {number}
-            </button>
-          </li>
-        );
+              <button
+                onClick={(e) => {
+                  onPageChanged(number);
+                }}
+              >
+                {number}
+              </button>
+            </li>
+          );
       })}
     </ul>
   );
