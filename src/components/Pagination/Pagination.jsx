7eb794e188558bc7ade13123b9f610385e81f4e9
Evgeniy Talko <etalko@gmail.com>
2021-02-12 16:01:55 +0300

    add: pages arr, prev/next  section buttons

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -9,39 +9,47 @@ const Pagination = ({
   sectionSize = 10,
 }) => {
   let pagesCount = Math.ceil(usersCount / usersPerPage);
+  const pagesArr = [...Array(pagesCount)].map((_,index) => index + 1 );
   let sectionsCount = pagesCount / sectionSize;
   const [currentSect, setCurrentSect] = setState(1);
   let minSectionPage = (currentSect - 1) * sectionSize - 1;
   let maxSectionPage = currentSect * sectionSize;
   return (
-    <ul className={s.pagination}>
-      // TODO: refactor array with undefined values to array with numbers.
-      {[...Array(pagesCount)]
-        .filter((_, index) => {
-          const page = index + 1;
-          return page > minSectionPage && page < maxSectionPage;
-        })
-        .map((_, index) => {
-          let number = index + 1;
-          return (
-            <li
-              className={
-                (number === currentPage && s.paginationActiveItem) +
-                " " +
-                s.paginationItem
-              }
-            >
-              <button
-                onClick={(e) => {
-                  onPageChanged(number);
-                }}
+    <>
+      { currentSect > 1 &&
+       <button onClick={() => setCurrentSect(currentSect - 1)} value="PREV"/> 
+      }
+      <ul className={s.pagination}>
+        // TODO: refactor array with undefined values to array with numbers.
+        {pagesArr
+          .filter((page) => {
+            return page > minSectionPage && page < maxSectionPage;
+          })
+          .map((_, index) => {
+            let number = index + 1;
+            return (
+              <li
+                className={
+                  (number === currentPage && s.paginationActiveItem) +
+                  " " +
+                  s.paginationItem
+                }
               >
-                {number}
-              </button>
-            </li>
-          );
-      })}
-    </ul>
+                <button
+                  onClick={(e) => {
+                    onPageChanged(number);
+                  }}
+                >
+                  {number}
+                </button>
+              </li>
+            );
+        })}
+      </ul>
+      { sectionsCount > currentSect &&
+       <button onClick={() => setCurrentSect(currentSect + 1)} value="NEXT"/> 
+      }
+    </>
   );
 };
 
