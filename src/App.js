b38ca2ee040798bfba6b77734bff1b88c9e40ec5
Evgeniy Talko <etalko@gmail.com>
2021-03-05 06:43:03 +0300

    refactor: app routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import "./index.css";
-import { Redirect, Route, withRouter } from "react-router-dom";
+import { Redirect, Route, Switch, withRouter } from "react-router-dom";
 import { compose } from "redux";
 import { connect } from "react-redux";
 import React, { useEffect } from "react";
@@ -12,11 +12,18 @@ import News from "./components/News/News";
 import Preloader from "./components/Preloader/Preloader";
 import withStoreAndRouter from "./hoc/withStoreAndRouter";
 import withSuspense from "./hoc/withSuspense";
+import NotFound from "./components/404/404";
 
-const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer"));
+const DialogsContainer = React.lazy(() =>
+  import("./components/Dialogs/DialogsContainer")
+);
 const Settings = React.lazy(() => import("./components/Settings/Settings"));
-const UsersContainer = React.lazy(() => import("./components/Users/UsersContainer"));
-const ProfileContainer = React.lazy(() => import("./components/Profile/ProfileContainer"));
+const UsersContainer = React.lazy(() =>
+  import("./components/Users/UsersContainer")
+);
+const ProfileContainer = React.lazy(() =>
+  import("./components/Profile/ProfileContainer")
+);
 const Login = React.lazy(() => import("./components/Login/Login"));
 
 const App = (props) => {
@@ -33,19 +40,25 @@ const App = (props) => {
       <HeaderContainer />
       <NavigationContainer />
       <div className="app-wrapper-content">
-        <Redirect
-          exact
-          from="/"
-          to="/profile"
-          render={() => <ProfileContainer />}
-        />
-        <Route exact path="/login" render={withSuspense(Login)} />
-        <Route exact path="/dialogs" render={withSuspense(DialogsContainer)} />
-        <Route path="/profile/:userId?" render={withSuspense(ProfileContainer)} />
-        <Route exact path="/users" render={withSuspense(UsersContainer)} />
-        <Route exact path="/news" component={News} />
-        <Route exact path="/music" component={Music} />
-        <Route exact path="/settings" component={withSuspense(Settings)} />
+        <Switch>
+          <Route
+            path="/profile/:userId?"
+            render={withSuspense(ProfileContainer)}
+          />
+          <Route exact path="/login" render={withSuspense(Login)} />
+          <Route
+            exact
+            path="/dialogs"
+            render={withSuspense(DialogsContainer)}
+          />
+          <Route exact path="/users" render={withSuspense(UsersContainer)} />
+          <Route exact path="/news" component={News} />
+          <Route exact path="/music" component={Music} />
+          <Route exact path="/settings" component={withSuspense(Settings)} />
+          <Route exact path="/profile" render={ProfileContainer} />
+          <Route exact path="/" render={() => <Redirect to="/profile" />} />
+          <Route path="*" component={NotFound} />
+        </Switch>
       </div>
     </div>
   );
