0103198c10ef1cbba0733ab6e2703c017314e770
Evgeniy Talko <etalko@gmail.com>
2022-01-26 20:42:35 +0300

    add(redux): redux-devtools integration

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,36 +1,45 @@
-import {combineReducers, createStore, applyMiddleware, Action} from "redux";
-import {reducer as formReducer} from "redux-form";
-import thunkMiddleware, {ThunkAction} from "redux-thunk";
-import appReducer from "./app-reducer";
-import authReducer from "./auth-reducer";
-import dialogsReducer from "./dialogs-reducer";
-import profileReducer from "./profile-reducer";
-import sidebarReducer from "./sidebar-reducer";
-import usersReducer from "./users-reducer";
+import { combineReducers, createStore, applyMiddleware, Action } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { reducer as formReducer } from 'redux-form'
+import thunkMiddleware, { ThunkAction } from 'redux-thunk'
+import appReducer from './app-reducer'
+import authReducer from './auth-reducer'
+import dialogsReducer from './dialogs-reducer'
+import profileReducer from './profile-reducer'
+import sidebarReducer from './sidebar-reducer'
+import usersReducer from './users-reducer'
 
 const rootReducer = combineReducers({
-  profilePage: profileReducer,
-  dialogsPage: dialogsReducer,
-  sidebar: sidebarReducer,
-  usersPage: usersReducer,
-  auth: authReducer,
-  form: formReducer,
-  app: appReducer,
-});
+    profilePage: profileReducer,
+    dialogsPage: dialogsReducer,
+    sidebar: sidebarReducer,
+    usersPage: usersReducer,
+    auth: authReducer,
+    form: formReducer,
+    app: appReducer,
+})
 
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunkMiddleware))
+)
 
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof store.getState>
 
 export type AppThunk<R = void, A extends Action = Action> = ThunkAction<
-  R,
-  RootState,
-  unknown,
-  A
+    R,
+    RootState,
+    unknown,
+    A
 >
 
-export type AppAsyncThunk<A extends Action = Action> = AppThunk<Promise<void>, A>
+export type AppAsyncThunk<A extends Action = Action> = AppThunk<
+    Promise<void>,
+    A
+>
 
-export type InferValuesTypes<T> = T extends {[key: string]: infer U} ? U : never;
+export type InferValuesTypes<T> = T extends { [key: string]: infer U }
+    ? U
+    : never
 
-export default store;
+export default store
