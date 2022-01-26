4b5c407cfcf2caff398f199be660587d03332c15
Evgeniy Talko <etalko@gmail.com>
2022-01-26 22:18:24 +0300

    REWRITE: initialize with vitejs

--- src/redux/app-selectors.ts
+++
@@ -1,5 +0,0 @@
-import {RootState} from "./redux-store";
-
-export function getAppIsInitializedState(state: RootState) {
-  return state.app.isInitialized;
-}
--- src/redux/sidebar-selectors.ts
+++
@@ -1,9 +0,0 @@
-import {RootState} from "./redux-store";
-
-export function getItemsState(state: RootState) {
-  return state.sidebar.items;
-}
-
-export function getWidgetsState(state: RootState) {
-  return state.sidebar.widgets;
-}
